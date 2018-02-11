5e68c776edad026328cb93217f7ed5b4240b3444
Elliot Forbes <elliot@elliots-mbp.home>
2018-02-11 11:39:32 +0000

    Migrated from old vue to new vue cli project

---
+++ imgur-frontend/src/components/HelloWorld.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="container">
+        <div class="row">
+            <div class="col l3">
+                <div class="img-card">
+                    <div class="img">
+                        <img src="http://lorempixel.com/400/200" alt="">
+                    </div>
+                    <div class="info">
+                        <h4>Elliot Forbes</h4>
+                        <p>10th Feb, 2018</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="img-card">
+                    <div class="img">
+                        <img src="http://lorempixel.com/400/200" alt="">
+                    </div>
+                    <div class="info">
+                        <h4>Elliot Forbes</h4>
+                        <p>10th Feb, 2018</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="img-card">
+                    <div class="img">
+                        <img src="http://lorempixel.com/400/200" alt="">
+                    </div>
+                    <div class="info">
+                        <h4>Elliot Forbes</h4>
+                        <p>10th Feb, 2018</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="img-card">
+                    <div class="img">
+                        <img src="http://lorempixel.com/400/200" alt="">
+                    </div>
+                    <div class="info">
+                        <h4>Elliot Forbes</h4>
+                        <p>10th Feb, 2018</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
